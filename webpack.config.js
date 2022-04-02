19f0895cd4d901c465f15d5eee3daf14217dd899
idjh263 <idjh263@gmail.com>
2022-04-02 12:14:47 -0400

    analyzer added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
     entry: './public/js/index.js', 
@@ -6,5 +8,21 @@ module.exports = {
         path: path.resolve(__dirname, 'public/js'), 
         filename: 'main.bundle.js'
     }, 
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static'
+          }), 
+          new WebpackPwaManifest({
+            name: "My Account Management",
+            short_name: "Budget",
+            description: "An app that allows you to view budget.", start_url: "../public/index.html",
+            background_color: "#dddddd", theme_color: "#ffffff", fingerprints: false,
+            inject: false,
+            icons: [{
+            src: path.resolve("public/icons/icon-512x512.png"), sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("public", "icons")
+            }] })
+    ],
     mode: 'development'
+   
 };
