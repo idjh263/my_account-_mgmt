94b4b24205d38554b00146deef78c372e6ec2f76
idjh263 <idjh263@gmail.com>
2022-04-03 17:32:17 -0400

    manifest json added

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect((process.env.MONGDB_URI || 'mongodb://localhost:27017/my_account-_mgmt', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
