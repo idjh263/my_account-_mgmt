2619bf4c4b14b190b40f7a4037cbc17ac33ab942
idjh263 <idjh263@gmail.com>
2022-04-03 11:43:45 -0400

    service worker added

--- service-worker.js
+++ service-worker.js
@@ -4,12 +4,54 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
         "./public/index.html", 
         "./public/css/styles.css",
-        // "./public/js/index.js",
-        "./public/js/main.bundle.js"
+        "./public/js/index.js",
+        "./public/js/idb.js",
+        //"./public/js/manifest.json",
+        "./public/icons/icon-72x72.png",
+        "./public/icons/icon-96x96.png",
+        "./public/icons/icon-128x128.png",
+        "./public/icons/icon-144x144.png",
+        "./public/icons/icon-152x152.png",
+        "./public/icons/icon-192x192.png",
+        "./public/icons/icon-384x384.png",
+        "./public/icons/icon-512x512.png"
     ];
 
-
 // Cache resources
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  });
+
+  // Delete outdated caches
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        // `keyList` contains all cache names under your username.github.io
+        // filter out ones that has this app prefix to create keeplist
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        // add current cache name to keeplist
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+  });
+
+
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
@@ -27,38 +69,7 @@ self.addEventListener('fetch', function (e) {
       // return request || fetch(e.request)
     })
   )
-})
-
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
-    })
-  )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create keeplist
-      let cacheKeeplist = keyList.filter(function(key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      // add current cache name to keeplist
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(
-        keyList.map(function(key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
-        })
-      );
-    })
-  );
 });
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const path = require('path');
+const webpack = require('webpack');const path = require('path');
+//const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-module.exports = {
+const config = {
     entry: './public/js/index.js', 
     output: {
         path: path.resolve(__dirname, 'public/js'), 
@@ -15,14 +16,19 @@ module.exports = {
           new WebpackPwaManifest({
             name: "My Account Management",
             short_name: "Budget",
-            description: "An app that allows you to view budget.", start_url: "../public/index.html",
-            background_color: "#dddddd", theme_color: "#ffffff", fingerprints: false,
+            description: "An app that allows you to view budget.", 
+            start_url: "../public/index.html",
+            background_color: "#dddddd", 
+            theme_color: "#ffffff", 
+            fingerprints: false,
             inject: false,
             icons: [{
             src: path.resolve("public/icons/icon-512x512.png"), sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join("public", "icons")
-            }] })
+            }] 
+        })
     ],
     mode: 'development'
    
 };
+module.exports = config;
