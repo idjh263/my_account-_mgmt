3820fdb95f528d26d308435cba7f1ba99d68a524
idjh263 <idjh263@gmail.com>
2022-04-03 17:37:55 -0400

    fixed bugs

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost:27017/my_account-_mgmt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/my_account-_mgmt', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
