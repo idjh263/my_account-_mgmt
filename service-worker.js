3b7cc4517a00cf9384393ccc551f16ab94d14bfd
idjh263 <idjh263@gmail.com>
2022-04-03 20:27:33 -0400

    updated folder

--- service-worker.js
+++ service-worker.js
@@ -2,18 +2,18 @@ const APP_PREFIX = 'MyAccountMgmt-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-        "./public/index.html", 
-        "./public/css/styles.css",
-        "./public/js/index.js",
-        "./public/js/idb.js",
-        "./public/icons/icon-72x72.png",
-        "./public/icons/icon-96x96.png",
-        "./public/icons/icon-128x128.png",
-        "./public/icons/icon-144x144.png",
-        "./public/icons/icon-152x152.png",
-        "./public/icons/icon-192x192.png",
-        "./public/icons/icon-384x384.png",
-        "./public/icons/icon-512x512.png"
+        "./index.html", 
+        "./assets/css/styles.css",
+        "./assets/js/index.js",
+        "./assets/js/idb.js",
+        "./assets/icons/icon-72x72.png",
+        "./assets/icons/icon-96x96.png",
+        "./assets/icons/icon-128x128.png",
+        "./assets/icons/icon-144x144.png",
+        "./assets/icons/icon-152x152.png",
+        "./assets/icons/icon-192x192.png",
+        "./assets/icons/icon-384x384.png",
+        "./assets/icons/icon-512x512.png"
     ];
 
 // Cache resources
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,56 @@
-const webpack = require('webpack');const path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 //const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 const config = {
-    entry: './public/js/index.js', 
+    entry: './assets/js/index.js', 
     output: {
-        path: path.resolve(__dirname, 'public/js'), 
-        filename: 'main.bundle.js'
-    }, 
+      filename: '[name].bundle.js',
+      path: `${__dirname}/dist`
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  esModule: false,
+                  name(file) {
+                    return '[path][name].[ext]';
+                  },
+                  publicPath(url) {
+                    return url.replace('../', '/assets/');
+                  }
+                }
+              },
+              {
+                loader: 'image-webpack-loader'
+              }
+            ]
+          }
+        ]
+      },
     plugins: [
         new BundleAnalyzerPlugin({
             analyzerMode: 'static'
           }), 
           new WebpackPwaManifest({
             name: "My Account Management",
-            short_name: "Budget",
+            short_name: "myaccount-management",
             description: "An app that allows you to view budget.", 
-            start_url: "../public/index.html",
+            start_url: "public/index.html",
             background_color: "#dddddd", 
             theme_color: "#ffffff", 
             fingerprints: false,
             inject: false,
+            crossorigin: 'use-credentials',
             icons: [{
-            src: path.resolve("public/icons/icon-512x512.png"), sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("public", "icons")
+            src: path.resolve("assets/icons/icon-512x512.png"), sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
             }] 
         })
     ],
